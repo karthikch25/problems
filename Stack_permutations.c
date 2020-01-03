beeb867e891102a5f33473d7da9afa4604ed36a3
Karthik Chalamalasetti <54052517+karthikch25@users.noreply.github.com>
2020-01-04 01:06:19 +0530

    Update Stack_permutations.c

--- Stack_permutations.c
+++ Stack_permutations.c
@@ -1,2 +1,8 @@
-
+/*Stack permutations problem
+Input: 1st array->1 2 3
+       2nd array->2 1 3
+Output: Valid
+Input: 1st array->1 2 3
+       2nd array->3 1 2
+Output: Invalid*/
 https://photos.app.goo.gl/GaMarjksnog2ioui8
